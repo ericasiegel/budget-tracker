33c35a998ce0856dabc1b8cf55842da7084ad386
Erica Siegel <siegel.erica@gmail.com>
2021-03-10 11:37:41 -0800

    added insatll to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,8 @@
+// global constants
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
 // list of files we will be caching
 const FILES_TO_CACHE = [
     "./index.html",
@@ -8,5 +13,17 @@ const FILES_TO_CACHE = [
 
 // install the service worker
 self.addEventListener('install', function(e) {
+    // tell the browser to wait until the work is complete before terminating the service worker
+    e.waitUntil(
+        // find a specific cache by name then add every file in the FILES_TO_CACHE array to the cache
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache: ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
 
+// activate the service worker
+self.addEventListener('activate', function(e) {
+    e.waitUntil
 })
