56f1676c871237ad4023a8f9a3840ce33cec4249
Erica Siegel <siegel.erica@gmail.com>
2021-03-10 11:27:41 -0800

    added files to cache to servie worker

---
+++ public/service-worker.js
@@ -0,0 +1,12 @@
+// list of files we will be caching
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./js/index.js",
+    "./js/idb.js",
+    "./css/styles.css",
+]
+
+// install the service worker
+self.addEventListener('install', function(e) {
+
+})
